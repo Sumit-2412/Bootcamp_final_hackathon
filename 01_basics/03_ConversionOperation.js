1f6dbd4a2efebe0c38082153d11e395e33efa532
Sumit. <daddu9880@gmail.com>
2024-08-07 16:29:01 +0000

    conersion

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,9 +1,26 @@
 let score=true
-console.log(typeof score)
-console.log(typeof (score))
+//console.log(typeof score)
+//console.log(typeof (score))
 
 let val=Number(score)
-console.log(typeof val)
-console.log(val)
+//console.log(typeof val)
+//console.log(val)
 
+//"33"->33
+//"33abc"->NaN
+//true->1,false->0
+//null->0
+//undefined->NaN
+
+let isLoggedin="sumit"
+let boolIsLoggedin=Boolean(isLoggedin)
+//console.log(boolIsLoggedin)
+
+// 1=>true,0=>false
+// ""=>false,"0"=>true,"sumit"=>true
+// Conversion to String
+let someNumber=55
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
 
